5aeec2dee21a28da858459a741b5ca46e28458e3
Ilya Fedin <fedin-ilja2010@ya.ru>
2025-04-27 02:37:17 +0400

    Get rid of packaged/non-packaged separation

--- Telegram/SourceFiles/_other/packer.h
+++ Telegram/SourceFiles/_other/packer.h
@@ -27,7 +27,7 @@ extern "C" {
 #include <openssl/evp.h>
 } // extern "C"
 
-#if defined Q_OS_WIN && !defined PACKER_USE_PACKAGED // use Lzma SDK for win
+#if 0 // use Lzma SDK for win
 #include <LzmaLib.h>
 #else
 #include <lzma.h>
